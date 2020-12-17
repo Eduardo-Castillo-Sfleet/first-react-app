d21992377df5bdf77ca5a20b33c0ce95af43fd9b
Eduardo-Castillo-Sfleet <eduardo.castillo@sfleet.mx>
2020-12-17 16:25:27 -0600

    creando primer component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+//0. Import libraries
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+//1. Create a component
+const App = () => {
+
+
+  return(
+    <div>
+      <h1>Hello React</h1>
     </div>
   );
 }
 
+
+//-1.Export components
 export default App;
